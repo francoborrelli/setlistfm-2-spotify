4b77e8a1c226d9f31826070d4572b270cb5e87f4
Franco Borrelli <francoborrelli96@outlook.com>
2024-09-18 12:42:09 -0300

    add new setlists

--- src/constants/sets/index.ts
+++ src/constants/sets/index.ts
@@ -1,9 +1,35 @@
-import { LOLLAPALOOZA_ARGENTINA_2025 } from './lolla2025';
+import {
+  FRIDAY_LOLLAPALOOZA_ARGENTINA_2025,
+  LOLLAPALOOZA_ARGENTINA_2025,
+  SATURDAY_LOLLAPALOOZA_ARGENTINA_2025,
+  SHORT_LOLLAPALOOZA_ARGENTINA_2025,
+  SUNDAY_LOLLAPALOOZA_ARGENTINA_2025,
+} from './lolla2025';
 
 export const SETS = {
   LOLLAPALOOZA_ARGENTINA_2025: {
     name: 'Lollapalooza Argentina 2025',
     artists: LOLLAPALOOZA_ARGENTINA_2025,
     description: 'Artists that will perform at Lollapalooza Argentina 2025',
+  },
+  SHORT_LOLLAPALOOZA_ARGENTINA_2025: {
+    name: 'Short Lollapalooza Argentina 2025',
+    artists: SHORT_LOLLAPALOOZA_ARGENTINA_2025,
+    description: 'Short list of artists that will perform at Lollapalooza Argentina 2025',
+  },
+  FRIDAY_LOLLAPALOOZA_ARGENTINA_2025: {
+    name: 'Friday Lollapalooza Argentina 2025',
+    artists: FRIDAY_LOLLAPALOOZA_ARGENTINA_2025,
+    description: 'Artists that will perform at Lollapalooza Argentina 2025 on Friday',
+  },
+  SATURDAY_LOLLAPALOOZA_ARGENTINA_2025: {
+    name: 'Saturday Lollapalooza Argentina 2025',
+    artists: SATURDAY_LOLLAPALOOZA_ARGENTINA_2025,
+    description: 'Artists that will perform at Lollapalooza Argentina 2025 on Saturday',
+  },
+  SUNDAY_LOLLAPALOOZA_ARGENTINA_2025: {
+    name: 'Sunday Lollapalooza Argentina 2025',
+    artists: SUNDAY_LOLLAPALOOZA_ARGENTINA_2025,
+    description: 'Artists that will perform at Lollapalooza Argentina 2025 on Sunday',
   },
 };
